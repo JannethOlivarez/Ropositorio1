9987b5d8ca925fdf9e13706c637cc4159550504d
Ricardo <Ricardo@Ricardo>
2016-10-02 16:21:09 -0500

    Cabezera

--- Includes/Diseno/CABEZERA.php
+++ Includes/Diseno/CABEZERA.php
@@ -1,17 +1,17 @@
 <?php
 
 $cabezera = '<div class="container">
-				<div class="col-xs-12 col-md-6">
-					<img class="img-responsive" src="../../Imagenes/LogoForestar.png"/>
-					<div class="container">
-						<div class="btn-group btn-group-justified btn-lg">
-							<ul class="nav nav-pills">
-								<li class="active">
-									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Lotes/LISTAL.PHP">Lotes</a>
-								</li>
-								<li class="active">
-									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Plantas/LISTA.PHP">Plantas</a>
-								</li>
+    <div class="col-xs-12 col-md-6">
+    <img class="img-responsive" src="../../Imagenes/LogoForestar.png"/>
+    <div class="container">
+    <div class="btn-group btn-group-justified btn-lg">
+    <ul class="nav nav-pills">
+    <li class="active">
+    <a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Lotes/LISTAL.PHP">Lotes</a>
+    </li>
+    <li class="active">
+    <a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Plantas/LISTA.PHP">Plantas</a>
+    </li>
 								<li class="active">
 									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Productos/LISTAP.PHP">Productos</a>
 								</li>
@@ -28,5 +28,17 @@ $cabezera = '<div class="container">
 						</div>
 					</div>
 				</div>
-			</div>';
+			</div>
+                        <div class="container" >
+                        <ul class="nav nav-tabs">
+  <li class="active"><a href="#">Inicio</a></li>
+  <li><a href="#">Perfil</a></li>
+  <li><a href="#">Mensajes</a></li>
+</ul>
+</div>
+                        '
+        
+        ;
+
+
 ?>
