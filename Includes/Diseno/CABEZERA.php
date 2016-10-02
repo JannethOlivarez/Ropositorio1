18dc62fcb2586a52f76310afd9d2837e2016f0a2
Ricardo <Ricardo@Ricardo>
2016-10-02 16:42:14 -0500

    Cabezera

--- Includes/Diseno/CABEZERA.php
+++ Includes/Diseno/CABEZERA.php
@@ -4,7 +4,7 @@ $cabezera = '<div class="container">
                  <div class="col-xs-12 col-md-6">
                       <img class="img-responsive" src="../../Imagenes/LogoForestar.png"/>
                       <div class="container" >
-                        <ul class="nav nav-pills"">
+                        <ul class="nav nav-pills">
                             <li class="active"><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/MENU.PHP">Inicio</a></li>
                             <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Lotes/LISTAL.PHP">Lotes </a></li>
                             <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Plantas/LISTA.PHP">Plantas</a></li>
