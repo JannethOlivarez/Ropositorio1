f657d486b40b4c624b1c0a17c2c3297f48a27843
Ricardo <Ricardo@Ricardo>
2016-10-02 16:37:47 -0500

    Cabezera

--- Includes/Diseno/CABEZERA.php
+++ Includes/Diseno/CABEZERA.php
@@ -1,42 +1,21 @@
 <?php
 
 $cabezera = '<div class="container">
-    <div class="col-xs-12 col-md-6">
-    <img class="img-responsive" src="../../Imagenes/LogoForestar.png"/>
-    <div class="container">
-    <div class="btn-group btn-group-justified btn-lg">
-    <ul class="nav nav-pills">
-    <li class="active">
-    <a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Lotes/LISTAL.PHP">Lotes</a>
-    </li>
-    <li class="active">
-    <a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Plantas/LISTA.PHP">Plantas</a>
-    </li>
-								<li class="active">
-									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Productos/LISTAP.PHP">Productos</a>
-								</li>
-								<li class="active">
-									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Aplicaciones/LISTAA.PHP">Aplicaciones de Productos</a>
-								</li>
-								<li class="active">
-									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/PlantasLotes/LISTAV.PHP">Siembras</a>
-								</li>
-                                                                <li class="active">
-									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Consultas/CONSULTAS.PHP">Consultas</a>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-                        <div class="container" >
-                        <ul class="nav nav-tabs">
-  <li class="active"><a href="#">Inicio</a></li>
-  <li><a href="#">Perfil</a></li>
-  <li><a href="#">Mensajes</a></li>
-</ul>
-</div>
-                        '
+                 <div class="col-xs-12 col-md-6">
+                      <img class="img-responsive" src="../../Imagenes/LogoForestar.png"/>
+                      <div class="container" >
+                        <ul class="nav nav-pills"">
+                            <li class="active"><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/MENU.PHP">Inicio</a></li>
+                            <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Lotes/LISTAL.PHP">Lotes </a></li>
+                            <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Plantas/LISTA.PHP">Plantas</a></li>
+                            <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Productos/LISTAP.PHP">Productos</a></li>
+                            <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Aplicaciones/LISTAA.PHP">Aplicaciones de Productos</a></li>
+                            <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/PlantasLotes/LISTAV.PHP">Siembras</a></li>
+                            <li><a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Consultas/CONSULTAS.PHP">Consultas</a></li>
+                        </ul>
+                       </div>
+                 </div> 
+            </div> '
         
         ;
 
