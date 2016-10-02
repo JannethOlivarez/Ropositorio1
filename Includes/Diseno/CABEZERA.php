ad57437b36ce97e20403f1a98cf9867b9758bde6
Ricardo <Ricardo@Ricardo>
2016-10-02 12:31:00 -0500

    Cabezera

--- Includes/Diseno/CABEZERA.php
+++ Includes/Diseno/CABEZERA.php
@@ -7,22 +7,22 @@ $cabezera = '<div class="container">
 						<div class="btn-group btn-group-justified btn-lg">
 							<ul class="nav nav-pills">
 								<li class="active">
-									<a href="http://localhost:8080/Ecuaforestar/Usuarios/Lotes/LISTAL.PHP">Lotes</a>
+									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Lotes/LISTAL.PHP">Lotes</a>
 								</li>
 								<li class="active">
-									<a href="http://localhost:8080/Ecuaforestar/Usuarios/Plantas/LISTA.PHP">Plantas</a>
+									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Plantas/LISTA.PHP">Plantas</a>
 								</li>
 								<li class="active">
-									<a href="http://localhost:8080/Ecuaforestar/Usuarios/Productos/LISTAP.PHP">Productos</a>
+									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Productos/LISTAP.PHP">Productos</a>
 								</li>
 								<li class="active">
-									<a href="http://localhost:8080/Ecuaforestar/Usuarios/Aplicaciones/LISTAA.PHP">Aplicaciones de Productos</a>
+									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Aplicaciones/LISTAA.PHP">Aplicaciones de Productos</a>
 								</li>
 								<li class="active">
-									<a href="http://localhost:8080/Ecuaforestar/Usuarios/PlantasLotes/LISTAV.PHP">Siembras</a>
+									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/PlantasLotes/LISTAV.PHP">Siembras</a>
 								</li>
                                                                 <li class="active">
-									<a href="http://localhost:8080/Ecuaforestar/Usuarios/Consultas/CONSULTAS.PHP">Consultas</a>
+									<a href="http://192.168.100.7:8080/Ecuaforestar/Usuarios/Consultas/CONSULTAS.PHP">Consultas</a>
 								</li>
 							</ul>
 						</div>
