fcf187a380cebcf7a9f19827531849f63eaa61a3
Janneth <you@example.com>
2017-03-08 17:25:18 -0500

    Cabezera

---
+++ usuarios/menu.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+
+        <link rel="stylesheet" href="/Ecuaforestar/includes/diseno/css/estilos.css"> 
+       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <style>
+            .carousel-inner > .item > img,
+            .carousel-inner > .item > a > img {
+                width: 70%;
+                margin: auto;
+            }
+            .item{
+                width: 550px;
+                height: 100%;
+            }
+
+        </style>
+    </head>
+    <body>
+
+        <img class="img-responsive" src="../imagenes/imagen2.png"/>
+        <div class="container">
+                               
+            <p   class="lead text-center text-danger">
+                <strong>Descubre un Nuevo Estilo para vivir mejor</strong>
+                </p>  
+            <p class="text-info text-center"> <strong>
+                    Ecuaforestar::Plantas Maravillosas. Dirección Vía Santo Domingo - Quinindé, Recinto El Limón
+                </strong>.</p>  
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h2 class="bg-success">Lotes</h2>
+                    <p>
+                        Registre Lotes con su respectivo número y estado 
+                                               
+                    </p>
+                    <img class="img-responsive" src="../imagenes/lotes.jpg"/>
+                    <p><a class="btn btn-success btn-lg" href="/Ecuaforestar/usuarios/lotes/listal.php">Acceder &raquo;</a></p>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="bg-success">Plantas</h2>
+                    <p> Registre y actualice datos de sus Nuevas plantas  </p>
+                    <img class="img-responsive" src="../Imagenes/m.png"/>
+                    <p><a class="btn btn-success btn-lg" href="/Ecuaforestar/usuarios/plantas/lista.PHP">Acceder &raquo;</a></p>
+                    <div class="clearfix"></div>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="bg-success">Productos</h2>
+                    <p> Registre y actualice datos de nuevos productos químicos u orgánicos   .</p>
+                    <img class="img-responsive" src="../Imagenes/productos.jpg"/>
+                    <p><a class="btn btn-success btn-lg" href="/Ecuaforestar/usuarios/productos/listap.php">Acceder &raquo;</a></p>
+<div class="clearfix"></div>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="bg-success">Registro de Siembras</h2>
+                    <p> Registre los datos de sus viveros vinculando el lote y la planta   </p>
+                    <img class="img-responsive" src="../imagenes/siembras.jpg"/>
+                    <p><a class="btn btn-success btn-lg" href="/Ecuaforestar/usuarios/plantasLotes/listav.php"> Acceder &raquo;</a></p>
+                <div class="clearfix"></div>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="bg-success">Aplicaciones de Productos</h2>
+                    <p> Registre los diferentes productos aplicados a las plantas .</p>
+                    <img class="img-responsive" src="../imagenes/aplicaciones.jpg"/>
+                    <p><a class="btn btn-success btn-lg " href="/Ecuaforestar/usuarios/aplicaciones/listaa.php">Acceder &raquo;</a></p>
+                <div class="clearfix"></div>
+                </div>
+                <div class="col-md-4">
+                    <h2 class="bg-success">Consultas</h2>
+                    <p> Consulte todos los datos de plantas y aplicaciones de productos registrados</p>
+                    <img class="img-responsive" src="../imagenes/consultas.png"/>
+                    <p><a class="btn btn-success btn-lg" href="/Ecuaforestar/usuarios/consultas/consultas.php">Acceder &raquo;</a></p>
+                <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+
+</html>
