dba02c0cf3366eee0f1ec69927b1c96ad6a06485
Janneth <you@example.com>
2017-03-08 17:22:25 -0500

    Cabezera

---
+++ includes/session.php
@@ -0,0 +1,22 @@
+<?php
+
+session_start();
+
+function logged_in() {
+
+    return isset($_SESSION['codigo']);
+}
+
+function confirmar_logged_in() {
+    if (!logged_in()) {
+        Redireccionar("http://localhost:8080/Ecuaforestar/usuarios/login.php");
+    }
+}
+//if (ini_get("session.use_cookies")) 
+//{
+//  $params = session_get_cookie_params();
+//  setcookie(session_name(), '', time() - 42000,
+//      $params["path"], $params["domain"],
+//      $params["secure"], $params["httponly"]);
+//}
+?>
